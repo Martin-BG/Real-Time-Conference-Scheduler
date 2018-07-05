7037403130a3cc783dc77ddc79108ad9c40aa638
MartinBG <MartinBG@abv.bg>
2018-07-06 00:39:41 +0300

    rearranged fields to match order in DTOs

--- rtcs/src/main/java/com/codexio/rtcs/entities/User.java
+++ rtcs/src/main/java/com/codexio/rtcs/entities/User.java
@@ -26,11 +26,11 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-    private String password;
-
     @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
 
     @ManyToMany(
             mappedBy = "attendances",
