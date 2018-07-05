2a03ccc7f48b27ac70f7ffdb7b0aa6de2fad7c72
Rusti73 <dadi@dadi.info>
2018-07-05 12:01:41 +0300

    Security - not completed

---
+++ rtcs/src/main/java/com/codexio/rtcs/configuration/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.codexio.rtcs.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.
+                jdbcAuthentication()
+                .usersByUsernameQuery(usersQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+.anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+}
--- rtcs/src/main/java/com/codexio/rtcs/service/UserService.java
+++ rtcs/src/main/java/com/codexio/rtcs/service/UserService.java
@@ -6,4 +6,6 @@ public interface UserService {
 
     void create(User user);
 
+    User findUserByEmail(String email);
+
 }
