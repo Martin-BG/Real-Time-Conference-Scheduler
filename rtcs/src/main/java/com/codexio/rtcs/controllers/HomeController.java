ca1c78507ef7ff272b509d1286a4f3f75f6b9701
MartinBG <MartinBG@abv.bg>
2018-07-06 01:24:13 +0300

    Added Home Controller

---
+++ rtcs/src/main/java/com/codexio/rtcs/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.codexio.rtcs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController extends BaseController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String hello() {
+        return "/viewAllConferences/index";
+    }
+}
