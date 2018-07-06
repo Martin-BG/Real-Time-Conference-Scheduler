b9fc345d4a655ec62fa95ba8da2fc1b3aec5a13f
MartinBG <MartinBG@abv.bg>
2018-07-06 03:35:36 +0300

    Fixed routes to home page

--- rtcs/src/main/java/com/codexio/rtcs/controllers/HomeController.java
+++ rtcs/src/main/java/com/codexio/rtcs/controllers/HomeController.java
@@ -2,12 +2,18 @@ package com.codexio.rtcs.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController extends BaseController {
 
-    @GetMapping(value = {"/", "/index"})
-    public String hello() {
-        return "/viewAllConferences/index";
+    @GetMapping(value = {"/home"})
+    public ModelAndView home() {
+        return super.view("/home");
+    }
+
+    @GetMapping(value = {"", "/"})
+    public ModelAndView index() {
+        return super.redirect("/home");
     }
 }
